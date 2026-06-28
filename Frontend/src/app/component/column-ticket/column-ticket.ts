4c5335e8bc7f90742226746f53946db24561c56c
Zsolt <5047426+bodzaital@users.noreply.github.com>
2026-06-28 09:27:56 +0200

    Set up components

---
+++ Frontend/src/app/component/column-ticket/column-ticket.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-column-ticket',
+	imports: [],
+	templateUrl: './column-ticket.html',
+	styleUrl: './column-ticket.css',
+})
+export class ColumnTicket {}
