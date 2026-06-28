69df47acdc90461a84ed5f9aad11e1af98e6872d
Zsolt <5047426+bodzaital@users.noreply.github.com>
2026-06-28 08:51:25 +0200

    Set up pages + routing

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Columns } from './page/columns/columns';
+import { NotFound } from './page/not-found/not-found';
+import { Ticket } from './page/ticket/ticket';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: "", component: Columns },
+	{ path: "ticket/:id", component: Ticket },
+	{ path: "**", component: NotFound }
+];
---
+++ Frontend/src/app/page/columns/columns.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-columns',
+	imports: [],
+	templateUrl: './columns.html',
+	styleUrl: './columns.css',
+})
+export class Columns {}
